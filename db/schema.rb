578f8c383f6f0463e840404c456e231afd1b6c5c
KTomoya-arch <kato.eng.dao@gmail.com>
2021-08-17 22:18:34 +0900

    post.rb association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_014102) do
+ActiveRecord::Schema.define(version: 2021_08_16_225604) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "images", null: false
+    t.text "body", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
