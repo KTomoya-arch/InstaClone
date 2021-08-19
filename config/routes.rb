df4ead55854a0b406a084d61b59eaa2f97047e4a
KTomoya-arch <kato.eng.dao@gmail.com>
2021-08-19 21:44:02 +0900

    CRUD機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'posts#index'
+  resources :posts
   resources :users, only: [:new, :create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
-  resources :posts
+  delete 'logout', to: 'user_sessions#destroy' 
 end
